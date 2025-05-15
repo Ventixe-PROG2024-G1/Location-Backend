1cadb949a1fb9423d6cf6658efa11afc981939ad
Limpan89 <lbroback89@gmail.com>
2025-05-16 01:34:52 +0200

    Innitial commit

---
+++ WebApi/Extensions/MappingExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace WebApi.Extensions
+{
+    public static class MappingExtensions
+    {
+        public static TDest MapTo<TDest>(this object source)
+        {
+            ArgumentNullException.ThrowIfNull(source, nameof(source));
+
+            TDest dest = Activator.CreateInstance<TDest>()!;
+
+            var sourceProps = source.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var destProps = dest.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var dp in destProps)
+            {
+                var sp = sourceProps.FirstOrDefault(p => p.Name == dp.Name && p.PropertyType == dp.PropertyType);
+
+                if (sp != null && dp.CanWrite)
+                {
+                    var value = sp.GetValue(source);
+                    dp.SetValue(dest, value);
+                }
+            }
+            return dest;
+        }
+    }
+}
