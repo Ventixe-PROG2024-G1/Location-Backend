2adb56866caed9002482364b150a1474c586df24
Limpan89 <lbroback89@gmail.com>
2025-05-16 02:10:19 +0200

    added services

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
 using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,13 @@ builder.Services.AddGrpc(o =>
     o.Interceptors.Add<ApiKeyInterceptor>();
 });
 
+builder.Services.AddMemoryCache();
+
+builder.Services.AddDbContext<LocationContext>(e => e.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+
 builder.Services.AddSingleton<ApiKeyInterceptor>();
+builder.Services.AddScoped<LocationCache>();
+builder.Services.AddScoped<LocationRepository>();
 
 var app = builder.Build();
 
